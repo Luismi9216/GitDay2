fe2a031cc81eec539b9ab44d024a496f906a7a94
airmo <airmo@10.0.0.136>
2022-08-01 19:24:43 -0400

    add encapsulation

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,4 +4,19 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Demo Test ....");
 	}
+	private String TeamG4;
+	public String getTeamG4() {
+		return TeamG4;
+	}
+	public void setTeamG4(String teamG4) {
+		TeamG4 = teamG4;
+	}
+	public int getTeamNum() {
+		return teamNum;
+	}
+	public void setTeamNum(int teamNum) {
+		this.teamNum = teamNum;
+	}
+	private int teamNum;
+	
 }
